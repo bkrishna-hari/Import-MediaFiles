859c2b0681652b99d25eeb5d757ef28fc2e913f1
Hari Krishna <bkrishna-hari@users.noreply.github.com>
2016-04-04 16:52:57 +0530

    Added AzureRm
    * Reading Automation's resource group details
    * Connecting AzureRM

--- MediaFiles-AssetsInput.ps1
+++ MediaFiles-AssetsInput.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 .DESCRIPTION
     This runbook creates all assets which required for Disk Integrity process.
      
@@ -34,63 +34,63 @@ workflow MediaFiles-AssetsInput
     Param
     (
         [parameter(Mandatory=$true, Position=1, HelpMessage="The name of the Azure Subscription")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$AzureSubscriptionName,
         
         [parameter(Mandatory=$true, Position=2, HelpMessage="The name of the StorSimple resource")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$ResourceName,
-                
+        
         [parameter(Mandatory=$true, Position=3, HelpMessage="The registration key for the StorSimple manager")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$StorSimRegKey,
         
         [parameter(Mandatory=$true, Position=4, HelpMessage="The storage account name in which the script will be stored")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$StorageAccountName,
         
         [parameter(Mandatory=$true, Position=5, HelpMessage="The access key for the storage account")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$StorageAccountKey,
         
         [parameter(Mandatory=$true, Position=6, HelpMessage="The storage container name in which the files (media) will be stored")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$StorageContainerName,
         
         [parameter(Mandatory=$true, Position=7, HelpMessage="The Device which has to be verified disk status")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$SourceDeviceName,
         
         [parameter(Mandatory=$true, Position=8, HelpMessage="The Device on which the containers are to be cloned")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$TargetDeviceName,
         
         [parameter(Mandatory=$true, Position=9, HelpMessage="The name of the Virtual machine which has to be used for mount the volumes & verify the disk status.")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$VMName,
         
         [parameter(Mandatory=$true, Position=10, HelpMessage="The Cloud service name where Virtual machine is running")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$VMServiceName,
         
         [parameter(Mandatory=$true, Position=11, HelpMessage="A comma separated string of volume containers present on the Device that need to be checked, ex - VolCon1,VolCon2")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$VolumeContainers,
         
         [parameter(Mandatory=$false, Position=12, HelpMessage="A comma separated string for Director names, ex - log*,Sam*")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$DirectoryNameFilter,
         
         [parameter(Mandatory=$false, Position=13, HelpMessage="A comma separated string for File names, ex - (sam).*(\.mp4),(Asset).*(\.mp4)")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$FileNameFilter,
 
         [parameter(Mandatory=$true, Position=14, HelpMessage="The name of the Aumation account name")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [String]$AutomationAccountName
     )
 	
-  	# Set empty string if input is null   
+    # Set empty string if input is null   
     If ([string]::IsNullOrEmpty($DirectoryNameFilter) -eq $false) {
         $DirectoryNameFilter = "*"
     }
@@ -98,7 +98,7 @@ workflow MediaFiles-AssetsInput
         $FileNameFilter = "*.*"
     }
 	   
-	# Add all new assets in collection object
+    # Add all new assets in collection object
     $NewAssetList = @()
     $AssetProp = @{ Name="AzureSubscriptionName"; Value=$AzureSubscriptionName; IsMandatory=$true; }
     $AssetObj = New-Object PSObject -Property $AssetProp
@@ -152,23 +152,23 @@ workflow MediaFiles-AssetsInput
     $AssetObj = New-Object PSObject -Property $AssetProp
     $NewAssetList += $AssetObj
 	
-	# Validate all mandatory parameters
-	InlineScript 
-	{
- 		$NewAssetList = $Using:NewAssetList
-		$ErrorMessage = [string]::Empty
+    # Validate all mandatory parameters
+    InlineScript 
+    {
+        $NewAssetList = $Using:NewAssetList
+        $ErrorMessage = [string]::Empty
         
-		foreach ($NewAssetData in $NewAssetList) {
-			If ($NewAssetData.IsMandatory -and [string]::IsNullOrEmpty($NewAssetData.Value)) { 
-	            $ErrorMessage += "$($NewAssetData.Name) cannot be blank. `n" 
-	        }
-		}
+        foreach ($NewAssetData in $NewAssetList) {
+            If ($NewAssetData.IsMandatory -and [string]::IsNullOrEmpty($NewAssetData.Value)) { 
+                $ErrorMessage += "$($NewAssetData.Name) cannot be blank. `n" 
+            }
+        }
 		
         # Display message
-		If ([string]::IsNullOrEmpty($ErrorMessage) -eq $false) {
-			throw $ErrorMessage
-		}
-	}
+        If ([string]::IsNullOrEmpty($ErrorMessage) -eq $false) {
+            throw $ErrorMessage
+        }
+    }
     
     # Fetch basic Azure automation variables
     $AzureCredential = Get-AutomationPSCredential -Name "AzureCredential"
@@ -179,36 +179,43 @@ workflow MediaFiles-AssetsInput
     
     # Connect to Azure
     Write-Output "Connecting to Azure"
-    $AzureAccount = Add-AzureAccount -Credential $AzureCredential      
-    $AzureSubscription = Select-AzureSubscription -SubscriptionName $AzureSubscriptionName          
+    $AzureAccount = Add-AzureRmAccount -Credential $AzureCredential      
+    $AzureSubscription = Select-AzureSubscription -SubscriptionName $AzureSubscriptionName  
     If (($AzureSubscription -eq $null) -or ($AzureAccount -eq $null))
     {
         throw "Unable to connect to Azure"
     }
     
+    # Read automation account resource group
+    try {
+		$ResourceGroupName = (Get-AzureRMAutomationAccount | where AutomationAccountName -eq $AutomationAccountName).ResourceGroupName
+    }
+    catch {
+        throw "Failed to read automation account's resource group"
+    }
     # Fetch asset list in Automation account
     try {
-        $AssetList = (Get-AzureAutomationVariable -AutomationAccountName $AutomationAccountName)  
+        $AssetList = (Get-AzureRMAutomationVariable -AutomationAccountName $AutomationAccountName -ResourceGroupName $ResourceGroupName)
     }
     catch {
         throw "The Automation account ($AutomationAccountName) is not found."
     }
 	
-	Write-Output "Initiating to create assets"
+    Write-Output "Initiating to create assets"
     foreach ($NewAssetData in $NewAssetList)
     {
         $AssetVariableName = $NewAssetData.Name
         $AssetValue = $NewAssetData.Value
 		
-		# Print asset name & value
-		Write-Output "$AssetVariableName : $AssetValue"
+        # Print asset name & value
+        Write-Output "$AssetVariableName : $AssetValue"
         
         If ($AssetList -ne $null -and (($AssetList) | Where-Object {$_.Name -eq $AssetVariableName}) -ne $null) {
-            $asset = Set-AzureAutomationVariable -AutomationAccountName $AutomationAccountName -Name $AssetVariableName -Encrypted $false -Value $AssetValue
+            $asset = Set-AzureRmAutomationVariable -AutomationAccountName $AutomationAccountName -Name $AssetVariableName -ResourceGroupName $ResourceGroupName -Value $AssetValue -Encrypted $false
             Write-Output "$AssetVariableName asset updated"
         }
         else {
-            $asset = New-AzureAutomationVariable -AutomationAccountName $AutomationAccountName -Name $AssetVariableName -Value $AssetValue -Encrypted $false
+            $asset = New-AzureRmAutomationVariable -AutomationAccountName $AutomationAccountName -Name $AssetVariableName -Value $AssetValue -Encrypted $false -ResourceGroupName $ResourceGroupName
             Write-Output "$AssetVariableName asset created"
         }
     }
