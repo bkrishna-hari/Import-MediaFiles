ac4696f6a06ba690674aa88bb61fb367fc8e9945
Hari Krishna <bkrishna-hari@users.noreply.github.com>
2016-12-20 17:09:00 +0530

    Added description of the script.

--- TriggerDataTransformationJob.ps1
+++ TriggerDataTransformationJob.ps1
@@ -1,4 +1,22 @@
-workflow TriggerDataTransformationJob
+<#
+.DESCRIPTION
+    This runbook triggers the Data Transformation Job. 
+    This runbook depends on C# dlls which handles the Data transformation calls.  
+     
+.ASSETS     
+    SubscriptionId: The Subscription Id of the Azure. 
+    TenantId: The Tenant Id (guid) of the Azure Active Directory (AAD) tenant where the service principal resides.
+    ClientId: The Client Id of AAD app which has permissions to KeyVault
+    ActiveDirectoryKey: Client Secret of AAD app which has permissions to KeyVault
+    ResourceGroupName: The Resource group name of the DMS
+    DataManagerName: The name of the DataManager Resource within the specified resource group.
+    JobDefinitionName: The name of the Job definition.
+
+.NOTES:
+
+#>
+
+workflow CallDMSJob
 {
     param
     (
